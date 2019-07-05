2995a74eec546399c3bb4de6745136e4465f5493
Kashirin Ivan <ivakashirin@yandex.ru>
2019-07-05 15:30:22 +0700

    todo list

---
+++ TodoList/script.js
@@ -0,0 +1,24 @@
+var inputValue = document.querySelector(".new-task");
+var list = document.querySelector('ul');
+
+list.addEventListener('click', function (evt) {
+    if (evt.target.valueOf().tagName === "BUTTON") {
+        console.log("удалить");
+        var dir = evt.target.parentNode;
+        dir.remove();
+    }
+}, false);
+
+function newTask() {
+    if (inputValue.value === "") {
+        alert("Задача не введена");
+        return;
+    }
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(inputValue.value));
+    var deleteButton = document.createElement('button');
+    deleteButton.appendChild(document.createTextNode("Удалить"));
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+    inputValue.value = "";
+}
