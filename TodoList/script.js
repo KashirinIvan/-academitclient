bce5a35df70d4c7570d5806c70d18b9a59227a76
Kashirin Ivan <ivakashirin@yandex.ru>
2019-07-08 11:42:32 +0700

    todo list

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,23 +1,27 @@
 var inputValue = document.querySelector(".new-task");
-var list = document.querySelector('ul');
+var list = document.querySelector("ul");
+var buttonNewTask = document.querySelector(".button-new-task");
 
-list.addEventListener('click', function (evt) {
-    if (evt.target.valueOf().tagName === "BUTTON") {
-        var dir = evt.target.parentNode;
-        dir.remove();
-    }
-}, false);
+function ready() {
+    list.addEventListener('click', function (evt) {
+        if (evt.target.valueOf().tagName === "BUTTON") evt.target.parentElement.remove();
+    }, false);
 
-function newTask() {
-    if (inputValue.value === "") {
-        alert("Задача не введена");
-        return;
-    }
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(inputValue.value));
-    var deleteButton = document.createElement('button');
-    deleteButton.appendChild(document.createTextNode("Удалить"));
-    li.appendChild(deleteButton);
-    list.appendChild(li);
-    inputValue.value = "";
+    buttonNewTask.addEventListener("click", function newTask() {
+        if (inputValue.value === "") {
+            alert("Задача не введена");
+            return;
+        }
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(inputValue.value));
+
+        var deleteButton = document.createElement("button");
+        deleteButton.appendChild(document.createTextNode("Удалить"));
+        li.appendChild(deleteButton);
+
+        list.appendChild(li);
+        inputValue.value = "";
+    });
 }
+
+document.addEventListener("DOMContentLoaded", ready);
