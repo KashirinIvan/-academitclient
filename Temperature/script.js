64a91bbc5348d92d39848f751a1eabfef42d8778
Kashirin Ivan <ivakashirin@yandex.ru>
2019-07-09 10:07:32 +0700

    temperature

--- Temperature/script.js
+++ Temperature/script.js
@@ -1,12 +1,12 @@
-var celsius = document.querySelector(".celsius");
-var kelvin = document.querySelector(".kelvin");
-var fahrenheit = document.querySelector(".fahrenheit");
-var convertButton = document.querySelector(".convert-button");
-
 function ready() {
+    var celsius = document.querySelector(".celsius");
+    var kelvin = document.querySelector(".kelvin");
+    var fahrenheit = document.querySelector(".fahrenheit");
+    var convertButton = document.querySelector(".convert-button");
+
     convertButton.addEventListener("click", function () {
         if (celsius.value.replace(/\s/g, '').length === 0 || isNaN(celsius.value)) {
-            alert('Нужно вести число!');
+            alert("Нужно вести число!");
         } else {
             kelvin.value = parseInt(celsius.value) + 273.15;
             fahrenheit.value = parseInt(celsius.value) * 9 / 5 + 32;
