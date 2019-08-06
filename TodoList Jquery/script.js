ddddf2fcf3187ff0e98796258944105d8fd02aa8
Kashirin Ivan <ivakashirin@yandex.ru>
2019-08-06 11:11:34 +0700

    todo list qjuery

--- TodoList Jquery/script.js
+++ TodoList Jquery/script.js
@@ -4,17 +4,13 @@ $(document).ready(function () {
         if (listItem.length > 0) {
             var buttonDelete = "<button>Удалить</button>";
             $("#list").append("<li>" + listItem + buttonDelete + "</li>");
-            $("#inputValue").val('');
+            $("#inputValue").val("");
         } else {
             alert("Задача не введена");
-            return;
         }
     });
 
-    $("#list").click(function (evt) {
-        if (evt.target.valueOf().tagName === "BUTTON") {
-            var child = evt.target.parentNode;
-            child.parentNode.removeChild(child);
-        }
+    $("#list").on("click", "li", function () {
+        $(this).remove();
     });
 });
