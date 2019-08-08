ec0a46266d0d9c50282e9d283e95417dadce217d
Kashirin Ivan <ivakashirin@yandex.ru>
2019-08-08 15:55:30 +0700

    todo list jquery

--- TodoList Jquery/script.js
+++ TodoList Jquery/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $("#append").click(function () {
         var listItem = $("#inputValue").val();
+        listItem = listItem.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
         if (listItem.length > 0) {
             var buttonDelete = "<button>Удалить</button>";
-            
             $("#list").append("<li>" + listItem + buttonDelete + "</li>");
             $("#inputValue").val("");
         } else {
